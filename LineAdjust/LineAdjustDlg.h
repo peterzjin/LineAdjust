bf6e68a1652f11732830020b47a1161b0f1c42e9
Peter Jin <peter.zjin@gmail.com>
2014-08-09 10:32:16 +0800

    Init Commit for LineAdjust
    Signed-off-by: Peter Jin <peter.zjin@gmail.com>

---
+++ LineAdjust/LineAdjustDlg.h
@@ -0,0 +1,31 @@
+
+// LineAdjustDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CLineAdjustDlg 对话框
+class CLineAdjustDlg : public CDialogEx
+{
+// 构造
+public:
+	CLineAdjustDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_LINEADJUST_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
